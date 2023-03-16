585411e48781075dbc12636a1a5594fc3c6d3d62
rocknegi <rocknegi53@gmail.com>
2023-03-16 08:46:07 +0100

    setup GooglePlacesAuto Use this package to get drop down suggetions of city names

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react';
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+
 export default function App() {
+  const [value, setValue] = useState(null);
+  useEffect(() => {
+    console.log(value)
+  }, [value])
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="p-5 flex flex-col">
+      <h1 className="pb-5 text-center text-3xl font-bold underline">
+        Weather API
+      </h1>
+      <div>
+        <GooglePlacesAutocomplete
+          apiKey={import.meta.env.VITE_MAPS_KEY}
+          selectProps={{
+            value,
+            onChange: setValue,
+          }}
+        />
+      </div>
+    </div>
   )
 }
